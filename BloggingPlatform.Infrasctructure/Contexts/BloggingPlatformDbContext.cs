fe14d22bba7a4e1ad02486b70a3893987b485b38
lucas <lucaspbicalho@yahoo.com.br>
2025-01-21 11:21:35 -0300

    Add DbContext using entity framework

---
+++ BloggingPlatform.Infrasctructure/Contexts/BloggingPlatformDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BloggingPlatform.Infrasctructure.Contexts
+{
+    public class BloggingPlatformDbContext : DbContext
+    {
+        public BloggingPlatformDbContext(DbContextOptions<BloggingPlatformDbContext> options) : base(options)
+        {
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
