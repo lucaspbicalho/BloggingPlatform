0072ff95fe84a6b46b5b7f34fe025b4c20c48825
lucas <lucaspbicalho@yahoo.com.br>
2025-01-21 11:27:44 -0300

    Create Service

---
+++ BloggingPlatform.Services/Services/BloggingPlatformService.cs
@@ -0,0 +1,31 @@
+﻿
+namespace BloggingPlatform.Services.Services
+{
+    public class BloggingPlatformService : IBloggingPlatformService
+    {
+        public bool Delete(int codVenda)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BlogPost GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BlogPost> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(BlogPost BlogPostVM)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int codVenda)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BloggingPlatform.Services/Services/IBloggingPlatformService.cs
@@ -0,0 +1,11 @@
+﻿namespace BloggingPlatform.Services
+{
+    public interface IBloggingPlatformService
+    {
+        public List<BlogPost> List();
+        public BlogPost GetById(Guid id);
+        public void Save(BlogPost BlogPostVM);
+        public bool Update(int codVenda);
+        public bool Delete(int codVenda);
+    }
+}
